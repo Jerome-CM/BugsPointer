0e8dd62c3b493639739082fb478fc9994e531597
Bouteveille jerome <formation@community-technologie.fr>
2023-03-23 18:10:59 +0100

    Initial commit

---
+++ src/main/java/com/bugspointer/BugspointerApplication.java
@@ -0,0 +1,13 @@
+package com.bugspointer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugspointerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugspointerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugspointer/BugspointerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugspointer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugspointerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
