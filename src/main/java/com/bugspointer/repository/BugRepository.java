b58ec6588548ed93ba2044c7fe3d39388a540c3c
Bouteveille jerome <formation@community-technologie.fr>
2023-03-27 11:03:10 +0200

    Init repositories

--- src/main/java/com/bugspointer/repository/BugRepository.java
+++ src/main/java/com/bugspointer/repository/BugRepository.java
@@ -1,2 +1,12 @@
-package com.bugspointer.repository;public interface BugRepository {
+package com.bugspointer.repository;
+
+import com.bugspointer.entity.Bug;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BugRepository extends CrudRepository<Bug, Long> {
+
+
+
 }
--- src/main/java/com/bugspointer/repository/CompanyRepository.java
+++ src/main/java/com/bugspointer/repository/CompanyRepository.java
@@ -1,11 +1,11 @@
 package com.bugspointer.repository;
 
-import com.bugspointer.entity.Compagny;
+import com.bugspointer.entity.Company;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompagnyRepository extends CrudRepository<Compagny, Long> {
+public interface CompanyRepository extends CrudRepository<Company, Long> {
 
 
 
