7d57c90a10dcb9569f72e3e5278509fa7e64447b
aferonramet2022 <amandine.feronramet2022@campus-eni.fr>
2023-05-15 16:54:41 +0200

    Front - Modification page modal message d'envoi

--- src/main/resources/static/js/modalControl.js
+++ src/main/resources/static/js/modalControl.js
@@ -3,6 +3,9 @@ const key = "Vw6QSo7nPHkhXk7pD2z69HJ3A"; /*Ajoutez votre clé public ici, entre 
 let btnopen = document.querySelector('#bugspointer_open_popup');
 let btnopenlogo = document.querySelector('#bugspointer_logo');
 let btnclose = document.querySelector('#bugspointer_close_popup');
+let btnenvoi = document.querySelector('#bugspointer-popup-btn-submit')
+let messageElement = document.getElementById("bugspointer-message-success");
+let section = document.getElementById("bugspointer-affichage")
 
 const publicKey=document.getElementById("key")
 publicKey.value = key;
@@ -19,9 +22,16 @@ function masquer(){
     document.querySelector('#bugspointer_popup').style.display = "none";
 }
 
+function message(){
+    event.preventDefault();
+    messageElement.textContent = "Merci pour votre envoi";
+    section.style.display = "none";
+}
+
 btnopen.addEventListener("click", afficher);
 btnopenlogo.addEventListener("click", afficher);
 btnclose.addEventListener("click", masquer);
+btnenvoi.addEventListener("click", message);
 
 document.getElementById("urlPointed").value=urlPointed;
 
