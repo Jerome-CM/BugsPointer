f63d64fc63477fc48042f058d53ab224f2f77b08
aferonramet2022 <amandine.feronramet2022@campus-eni.fr>
2023-05-16 14:37:54 +0200

    Front - Rectification page modal message d'envoi formulaire envoyé

--- src/main/resources/static/js/modalControl.js
+++ src/main/resources/static/js/modalControl.js
@@ -3,11 +3,12 @@ const key = "Vw6QSo7nPHkhXk7pD2z69HJ3A"; /*Ajoutez votre clé public ici, entre 
 let btnopen = document.querySelector('#bugspointer_open_popup');
 let btnopenlogo = document.querySelector('#bugspointer_logo');
 let btnclose = document.querySelector('#bugspointer_close_popup');
-let btnenvoi = document.querySelector('#bugspointer-popup-btn-submit')
+let btnenvoi = document.querySelector('#bugspointer-popup-btn-submit');
+let form = document.querySelector('.bugspointer-popup-container');
 let messageElement = document.getElementById("bugspointer-message-success");
-let section = document.getElementById("bugspointer-affichage")
+let section = document.getElementById("bugspointer-affichage");
 
-const publicKey=document.getElementById("key")
+const publicKey=document.getElementById("key");
 publicKey.value = key;
 
 var urlPointed = window.location.href;
@@ -23,15 +24,28 @@ function masquer(){
 }
 
 function message(){
-    event.preventDefault();
-    messageElement.textContent = "Merci pour votre envoi";
+    messageElement.innerHTML = "Merci pour votre envoi";
     section.style.display = "none";
 }
 
 btnopen.addEventListener("click", afficher);
 btnopenlogo.addEventListener("click", afficher);
 btnclose.addEventListener("click", masquer);
-btnenvoi.addEventListener("click", message);
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let formData = new FormData(this);
+
+    fetch(this.action, {
+        method: this.method,
+        body: formData
+    }).then(function (response){
+        if (response.ok) {
+            message();
+        }
+    }).catch(function (error) {
+        console.error("Une erreur s'est produite lors de l'envoi du formulaire", error)
+    })
+});
 
 document.getElementById("urlPointed").value=urlPointed;
 
