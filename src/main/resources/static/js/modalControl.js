07aa6b8311af3ee49f53d41836d8d0019be104b5
aferonramet2022 <amandine.feronramet2022@campus-eni.fr>
2023-05-24 18:06:26 +0200

    Front - ajout du bouton Pointer un bug JS - ajout des eventListener sur la mouseover et mouseout pour sélectionner la balise et ajouter un cadre autour + balise.click pour ajouter la balise au formulaire (à terminer) et arreter les eventListener Reste des petits problèmes (essai avec le remove btnpointer.click mais ne permet plus de cliquer sur pointer un bug donc pas de repointage)

--- src/main/resources/static/js/modalControl.js
+++ src/main/resources/static/js/modalControl.js
@@ -5,6 +5,7 @@ const key = "Vw6QSo7nPHkhXk7pD2z69HJ3A"; /*Ajoutez votre clé public ici, entre 
 let btnopen = document.querySelector('#bugspointer_open_popup');
 let btnopenlogo = document.querySelector('#bugspointer_logo');
 let btnclose = document.querySelector('#bugspointer_close_popup');
+let btnpointer = document.querySelector('#bugspointer-popup-btn-pointer');
 let form = document.querySelector('.bugspointer-popup-container');
 let messageElement = document.getElementById("bugspointer-message-success");
 let section = document.getElementById("bugspointer-affichage");
@@ -12,6 +13,9 @@ let urlPointed = window.location.href;
 let os = document.getElementById('bugspointer-popup-os');
 let browser = document.getElementById('bugspointer-popup-browser');
 let screen = document.getElementById('bugspointer-popup-screen');
+let pointerSelected = document.getElementById('bugspointer-popup-pointer-bug');
+const balisePointer = document.querySelectorAll('*:not(body):not(html)');
+const backgroundPointer = "rgba(128,197,234,0.5)";
 const publicKey = document.getElementById("key");
 
 publicKey.value = key;
@@ -23,7 +27,7 @@ browser.value = browserWithVersion;
 
 /*dimension();*/
 
-document.getElementById("urlPointed").value=urlPointed;
+document.getElementById("urlPointed").value = urlPointed;
 
 /* Free Modal */
 
@@ -91,12 +95,117 @@ function get_browser() {
     return 'undefined';
 }
 
+/* Point a bug */
+
+let pointerActive = false;
+let iteration = 0;
+
+function pointer(){
+    /*pointerActive = true;
+
+    masquer();
+
+    for(let i = 0; i < balisePointer.length; i++){
+        let balise = balisePointer[i];
+        let originalBackgroundColor = getComputedStyle(balise).backgroundColor;
+        balise.addEventListener('mouseover', handleMouseOver);
+        balise.addEventListener('mouseout', handleMouseOut);
+        if (document.querySelector('#bugspointer_popup').style.display === "flex") {
+                    break;
+                }
+
+        function handleMouseOver(){
+            if(pointerActive) {
+                modifBackground(balise, backgroundPointer);
+                balise.addEventListener('click', () => {
+                    pointerSelected.value = handleClick();
+                });
+            }
+        }
+
+        function handleMouseOut() {
+            if (pointerActive) {
+                modifBackground(balise, originalBackgroundColor);
+            }
+        }
+
+        function handleClick() {
+            pointerActive = false;
+            modifBackground(balise, originalBackgroundColor);
+
+            pointerSelected.value = balise.textContent;
+            afficher();
+            iteration++;
+            console.log(iteration);
+            return balise.textContent;
+        }
+
+
+
+    }
+
+    if (!pointerActive){
+        return;
+    }*/
+
+    masquer();
+
+    let styleInitial;
+    let pointerActive;
+
+    document.addEventListener('mouseover', handleMouseOver);
+    document.addEventListener('mouseout', handleMouseOut);
+
+    function handleMouseOver(event) {
+        let balise = event.target.closest("*:not(body):not(html)");
+        if (balise != null) {
+            styleInitial = getComputedStyle(balise).border;
+            balise.style.border = '3px solid #FF0000';
+            balise.addEventListener('click', handleClick);
+        }
+        /*console.log("Balise survolée : ", balise)*/
+
+
+    }
+    function handleMouseOut(event) {
+        let balise = event.target.closest("*:not(body):not(html)");
+        if (balise != null) {
+            balise.style.border = styleInitial;
+        }
+    }
+    function handleClick(event){
+        /*event.preventDefault();*/
+        event.stopPropagation();
+        let balise = event.target.closest("*:not(body):not(html)");
+        if (balise != null) {
+            pointerSelected.value = balise.textContent;
+            console.log(balise);
+            document.removeEventListener('mouseover', handleMouseOver);
+            document.removeEventListener('mouseout', handleMouseOut);
+            balise.removeEventListener('click', handleClick);
+            /*btnpointer.removeEventListener("click", pointer);*/
+            balise.style.border = styleInitial;
+            afficher();
+        }
+
+    }
+    iteration++;
+    console.log(iteration);
+
+}
+
+
+
+function modifBackground(balise, backgroundColor){
+    balise.style.backgroundColor = backgroundColor;
+}
 
 /* Events Listener */
 
 btnopen.addEventListener("click", afficher);
 btnopenlogo.addEventListener("click", afficher);
-btnclose.addEventListener("click", masquer);
+btnclose.addEventListener("mousedown", masquer);
+btnpointer.addEventListener("click", pointer);
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     let formData = new FormData(this);
