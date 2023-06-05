01ac00c6df9eaba2e564540658e0fdf21e9835e2
Bouteveille jerome <formation@community-technologie.fr>
2023-06-05 22:42:26 +0200

    Maj mail et sondage

--- src/main/java/com/bugspointer/service/implementation/MailService.java
+++ src/main/java/com/bugspointer/service/implementation/MailService.java
@@ -47,7 +47,7 @@ public class MailService {
                 "       <table border='0' cellpadding='0' cellspacing='0' >" +
                 "           <tr>" +
                 "               <td align='center' style='padding: 10px;'>" +
-                "                   <a href='"+ ADRESSE +"confirmRegister/"+ publicKey + "' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Confirmer</a>" +
+                "                   <a href='"+ ADRESSE +"confirmRegister/"+ publicKey + "' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: orange;'>Confirmer</a>" +
                 "               </td>" +
                 "           </tr>" +
                 "       </table>" +
@@ -110,7 +110,7 @@ public class MailService {
                         "       <table border='0' cellpadding='0' cellspacing='0' >" +
                         "           <tr>" +
                         "               <td align='center' style='padding: 10px;'>" +
-                        "                   <a href='"+ ADRESSE +"features/' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Je veux en voir plus</a>" +
+                        "                   <a href='"+ ADRESSE +"features/' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: orange;'>Je veux en voir plus</a>" +
                         "               </td>" +
                         "           </tr>" +
                         "       </table>" +
@@ -174,7 +174,7 @@ public class MailService {
                         "       <table border='0' cellpadding='0' cellspacing='0' >" +
                         "           <tr>" +
                         "               <td align='center' style='padding: 10px;'>" +
-                        "                   <a href='"+ ADRESSE +"/features/' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Je veux en voir plus</a>" +
+                        "                   <a href='"+ ADRESSE +"/features/' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: orange;'>Je veux en voir plus</a>" +
                         "               </td>" +
                         "           </tr>" +
                         "       </table>" +
@@ -236,7 +236,7 @@ public class MailService {
                         "       <table border='0' cellpadding='0' cellspacing='0' >" +
                         "           <tr>" +
                         "               <td align='center' style='padding: 10px;'>" +
-                        "                   <a href='"+ ADRESSE +"resetPassword/"+ publicKey + "' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Réinitialiser mon mot de passe</a>" +
+                        "                   <a href='"+ ADRESSE +"resetPassword/"+ publicKey + "' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: orange;'>Réinitialiser mon mot de passe</a>" +
                         "               </td>" +
                         "           </tr>" +
                         "       </table>" +
@@ -289,17 +289,19 @@ public class MailService {
 
 
         // Paramètres du destinataire
-        String subject = "Bugspointer - Nouveau Bug (Test)";
+        String subject = "Bugspointer - Votre rapport de la page de test";
 
         // Contenu HTML de l'email
         String htmlContent =
-                "<html>" +
-                        "  <body>" +
-                        "    <h1> Vous venez d'envoyer un rapport de bug depuis la page de test du site bugspointer.com </h1>" +
-                        "      <table>" +
+                "<html style='background-color:#F0F1F3; font-size:16px' >" +
+                        "  <body >" +
+                        "    <h1> Améliorez votre site web avec Bugspointer - Obtenez des détails approfondis sur vos rapports de bugs ! </h1>" +
+                        "    <p>Cher utilisateur de Bugspointer, <br>" +
+                        "    Nous avons bien reçu votre rapport de bug depuis la page de test de notre site bugspointer.com, et nous tenons à vous remercier pour votre intérêt</p><br><br>" +
+                        "      <table width='65%' align='center' style='background-color: #FFFFFF;'>" +
                         "        <thead>" +
                         "        <tr>" +
-                        "          <th>Pour une utilisation gratuite vous recevrez ce type de mail :</th>" +
+                        "          <th>Voici un aperçu de ce que vous obtiendrez pour une utilisation gratuite :</th>" +
                         "          </tr>" +
                         "        </thead>" +
                         "        <tbody>" +
@@ -310,10 +312,10 @@ public class MailService {
                         "            <p>URL concernée : "+ bugTest.getUrl() +"</p><br>" +
                         "            <p>Description du bug : <br>" + bugTest.getDescription() +"</p><br>" +
                         "            <p>Afin d'avoir plus d'informations sur ce bug, vous pouvez changer votre abonnement en cliquant sur le bouton ci-dessous:" +
-                        "            <table border='0' cellpadding='0' cellspacing='0' >" +
+                        "            <table border='0' cellpadding='0' cellspacing='0' align='center'>" +
                         "              <tr>" +
                         "                <td align='center' style='padding: 10px;'>" +
-                        "                  <a href='"+ ADRESSE +"features/' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Je veux en voir plus</a>" +
+                        "                  <a href='"+ ADRESSE +"features/' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: orange;'>Je veux en voir plus</a>" +
                         "                </td>" +
                         "              </tr>" +
                         "            </table>" +
@@ -323,7 +325,7 @@ public class MailService {
                         "        </tbody>" +
                         "        <thead>" +
                         "            <tr>" +
-                        "              <th>Voici le niveau de détail que vous recevrez pour une utilisation payante : </th>" +
+                        "              <th>Voici un aperçu de ce que vous obtiendrez en mettant à niveau votre abonnement : </th>" +
                         "          </tr>" +
                         "         </thead>" +
                         "          <tbody>" +
@@ -332,60 +334,69 @@ public class MailService {
                         "                   <h1>Nouveau Bug</h1>" +
                         "                   <table>" +
                         "                       <tr>" +
-                                "                <th>URL concernée : </th>" +
+                                "                <th width='170px' text-align='right'>URL concernée : </th>" +
                                 "                <td>"+ bugTest.getUrl() +"</td>" +
                                 "              </tr>" +
                                 "              <tr>" +
-                                "                <th>Date du rapport : </th>" +
+                                "                <th width='170px' text-align='right'>Date du rapport : </th>" +
                                 "                <td>"+ bugTest.getDateCreation() +"</td>" +
                                 "              </tr>" +
                                 "              <tr>" +
-                                "                <th>Description du bug : </th>" +
+                                "                <th width='170px' text-align='right'>Description du bug : </th>" +
                                 "                <td>" + bugTest.getDescription() +"</td>" +
                                 "              </tr>" +
                                 "              <tr>" +
-                                "                <th>OS utilisateur : </th>" +
+                                "                <th width='170px' text-align='right'>OS utilisateur : </th>" +
                                 "                <td>"  + bugTest.getOs() +"</td>" +
                                 "              </tr>" +
                                 "              <tr>" +
-                                "                <th>Browser utilisateur : </th>" +
+                                "                <th width='170px' text-align='right'>Navigateur : </th>" +
                                 "                <td>"  + bugTest.getBrowser() +"</td>" +
                                 "              </tr>" +
                                 "              <tr>" +
-                                "                <th>Taille écran utilisateur : </th>" +
+                                "                <th width='170px' text-align='right'>Taille de l'écran : </th>" +
                                 "                <td>"  + bugTest.getScreenSize() +"</td>" +
                                 "              </tr>" +
                                 "              <tr>" +
-                                "                <th>Code HTML sélectionné : </th>" +
+                                "                <th width='170px' text-align='right'>Code HTML sélectionné : </th>" +
                                 "                <td><code>"  + bugTest.getCodeLocation() +"</code></td>" +
                                 "              </tr>" +
-                        "                   </table>" +
-                                "            <p>Vous pouvez retrouver ces détails dans votre Dashboard en cliquant sur le bouton ci-dessous:" +
-                                "            <table border='0' cellpadding='0' cellspacing='0' >" +
+                                "               <tr>" +
+                                "               <th>La balise pointé par vos soins est maintenant identifiable par la classe : bugspointer-pointed-balise</th>" +
+                                "              </tr>" +
+                                "            </table>" +
+                                "            <p>Imaginez la valeur que vous pourriez obtenir en ayant accès à ces détails précis pour tous vos rapports de bugs. Notre offre payante vous permettra de prendre des décisions éclairées, d'améliorer rapidement votre site et de fournir une expérience utilisateur exceptionnelle. <br><br>" +
+                                "            Vous pouvez également consulter votre Dashboard personnalisé pour avoir une vue d'ensemble complète de tous vos rapports de bugs et de leurs détails. C'est un outil puissant qui vous aidera à suivre les progrès de résolution des problèmes." +
+                                "            <table align='center' border='0' cellpadding='0' cellspacing='0' >" +
                                 "              <tr>" +
                                 "                <td align='center' style='padding: 10px;'>" +
-                                "                  <a href='"+ ADRESSE +"app/private/dashboard' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Voir mon Dashboard</a>" +
+                                "                  <a href='"+ ADRESSE +"app/private/dashboard' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: orange;'>Voir mon Dashboard</a>" +
                                 "                </td>" +
+                                "              </tr><br><br>" +
+                                "              <tr>" +
+                                "               <th>Merci d'avoir testé notre application, aidez-nous à nous améliorer encore plus en évaluant nos services grâce au bouton ci-dessous : </th>" +
                                 "              </tr>" +
                                 "            </table>" +
                                 "          </td>" +
                         "               </tr>" +
-                        "               </body>" +
-                        "               <thead>" +
-                        "                  <tr>" +
-                        "                     <th>Merci d'avoir testé notre application, aidez-nous à nous améliorer encore plus en cliquant sur le bouton ci-dessous : </th>" +
-                        "                  </tr>" +
-                        "               </thead>" +
-                        "               <tbody>" +
+
                         "                  <tr>" +
                         "                   <td>" +
-                        "                       <table border='0' cellpadding='0' cellspacing='0' >" +
+                        /*"                       <table border='0' cellpadding='0' cellspacing='0' >" +*/
                         "                       <tr>" +
-                        "                       <td align='center' style='padding: 10px;'>" +
-                        "                           <a href='"+ ADRESSE +"sondage' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 20px; color: white; text-decoration: none; background-color: orange;'>Donner mon avis</a>" +
+                        "                       <td align='center' style='padding: 10px'>" +
+                        "                           <a href='"+ ADRESSE +"sondage' style='display: inline-block; padding: 10px 20px; border-radius: 5px; font-size: 18px; color: white; text-decoration: none; background-color: #00E676;' box-shadow='0 0 5px lightgrey'>Donner mon avis</a>" +
                         "                       </td>" +
                         "                   </tr>" +
-                        "            </table>" +
+/*                        "            </table>" +*/
+                        "        <tr>" +
+                        "         <td> " +
+                    "               <p>Si vous avez des questions ou avez besoin d'une assistance supplémentaire, n'hésitez pas à nous contacter. Notre équipe est là pour vous aider.\n" +
+                    "               <br><br>" +
+                    "               Cordialement,<br>" +
+                    "               L'équipe Bugspointer</p>" +
+                        "         </td>" +
+                        "        </tr>" +
                         "       </tbody>" +
                         "      </table>" +
                         "  </body>" +
