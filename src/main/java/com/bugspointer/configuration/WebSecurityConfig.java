4abdd55a1871d2d5465903cbcee9254e2b4a088e
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2026-05-06 07:31:45 +0200

    Handle rejected HTTP methods cleanly

--- src/main/java/com/bugspointer/configuration/WebSecurityConfig.java
+++ src/main/java/com/bugspointer/configuration/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
+import org.springframework.security.web.firewall.RequestRejectedHandler;
 
 
 @Configuration
@@ -33,6 +35,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public RequestRejectedHandler requestRejectedHandler() {
+        return new HttpStatusRequestRejectedHandler(400);
     }
 
     @Override
